fdd0dcc05494eeadba7417882536898f3c2a768a
Parkkeo1 <keonp2@illinois.edu>
2018-07-23 10:48:54 -0400

    rough no-CSS implementation of steamid html form

--- cspy_reactjs_website/src/App.js
+++ cspy_reactjs_website/src/App.js
@@ -6,11 +6,45 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <SteamIdForm />
         <div className="footer">
           <a className="pr-4" href="https://github.com/Parkkeo1/CS-Py" target="_blank">What Is CS-Py?</a>
           <a className="pl-4" href="https://github.com/Parkkeo1" target="_blank">Developed By @Parkkeo1</a>
         </div>
       </div>
+    );
+  }
+}
+
+class SteamIdForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    console.log(this.state.value);
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Your SteamID64:
+          <input placeholder='Ex. 76561198158189084' type="text" value={this.state.value} onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="View Match Stats" />
+      </form>
     );
   }
 }

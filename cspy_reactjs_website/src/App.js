9550de08d229733acd8455cec21791def12d68cf
Parkkeo1 <keonp2@illinois.edu>
2018-07-23 09:38:23 -0400

    setup

---
+++ cspy_reactjs_website/src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="footer">
+          <a className="pr-4" href="https://github.com/Parkkeo1/CS-Py" target="_blank">What Is CS-Py?</a>
+          <a className="pl-4" href="https://github.com/Parkkeo1" target="_blank">Developed By @Parkkeo1</a>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
